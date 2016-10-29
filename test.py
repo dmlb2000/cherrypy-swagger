252baaa265a2c346c63d901936b5f7c2d29f512c
David Brown <dmlb2000@gmail.com>
2016-10-29 16:50:41 -0700

    fix pylint issues

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """This is the main test python module for executing all tests."""
 from unittest import main
+# pylint: disable=unused-import
 from hello.test.hello_test import TestHello
 from example.string_generator_test import TestStringGenerator
 
